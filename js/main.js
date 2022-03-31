9971052dd1baf3e58b16e891373aa3b4d489c103
Samiul Haque Sohan <samiulhsohan@gmail.com>
2022-03-31 23:08:17 +0300

    add age

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 import {_} from './selector.js'
 import {startGame, renderQuestion} from './utils.js'
 
+window.age = 0
 window.feeling = {}
 
 _('#start').addEventListener('click', () => {
   startGame()
+})
+
+_('#age-form').addEventListener('submit', e => {
+  e.preventDefault()
+  const formData = new FormData(e.target)
+  const age = formData.get('age')
+  if (!age) return
+  window.age = age
   renderQuestion(window.questions[0])
 })
 ;(() => {
