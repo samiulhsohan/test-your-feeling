06948f8087eb70da107573a65c3e4b2287406207
Samiul Haque Sohan <samiulhsohan@gmail.com>
2022-03-31 23:30:35 +0300

    add new styles

--- js/utils.js
+++ js/utils.js
@@ -1,17 +1,23 @@
 import {handleAnswer} from './answer.js'
 import {_} from './selector.js'
 
+const backgroundColors = {
+  '02': '#d79496',
+  '03': '#e8ba80',
+}
+
 export const startGame = () => {
   _('#intro').style.display = 'none'
   _('#question').style.display = 'block'
 }
 
 export const renderQuestion = question => {
-  const {styles, options} = question
+  const {styleVariant, options} = question
 
-  _('.blur-layer').style.backgroundColor = styles.backgroundColor
+  _('.blur-layer').style.backgroundColor = backgroundColors[styleVariant]
   const test = _('#test')
-  test.innerHTML = ''
+
+  setEffectLayerImages(styleVariant)
 
   let optionsHtml = ''
   const questionHtml = `<p class="question-text">${question.question}</p>`
@@ -33,4 +39,15 @@ export const renderQuestion = question => {
       handleAnswer(question, option)
     })
   })
+}
+
+const setEffectLayerImages = styleVariant => {
+  const effectLayer = _('#effect-layer')
+  const images = `
+    <img class="bounce-1" src="img/${styleVariant}/01.png" alt="" />
+    <img class="bounce-2" src="img/${styleVariant}/02.png" alt="" />
+    <img class="bounce-3" src="img/${styleVariant}/03.png" alt="" />
+    <img class="bounce-4" src="img/${styleVariant}/04.png" alt="" />
+  `
+  effectLayer.innerHTML = images
 }
