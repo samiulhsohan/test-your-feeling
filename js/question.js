b9286a7393be0ad80fc593c012e433dbc3c279ea
Samiul Haque Sohan <samiulhsohan@gmail.com>
2022-04-01 12:43:41 +0300

    update style

--- js/question.js
+++ js/question.js
@@ -7,30 +7,9 @@ import {
   enableOptions,
 } from './utils.js'
 
-const backgroundColors = {
-  '01': '#64bbd6',
-  '02': '#d79496',
-  '03': '#e8ba80',
-  '04': '#bed5e5',
-  '05': '#ebb671',
-  '06': '#da8e98',
-  '07': '#d79496',
-  '08': '#555692',
-  '09': '#fcba4d',
-  10: '#73a4b5',
-  11: '#d6646a',
-  12: '#6f8eaf',
-  13: '#33a9b1',
-  14: '#dd7c72',
-  15: '#ebb671',
-  16: '#daa962',
-  17: '#d6aa73',
-}
-
 export const renderQuestion = question => {
   const {styleVariant, options, type, color} = question
 
-  _('.blur-layer').style.backgroundColor = backgroundColors[styleVariant]
   const test = _('#test')
 
   setTimeout(() => {
